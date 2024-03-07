e10ea78f026fdf788f8c438c74193894f4996078
Alex Mitchell <shamsup@users.noreply.github.com>
2024-03-07 02:44:57 -0800

    add referral link to demo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -73,7 +73,7 @@ export default function Index() {
               {
                 src: railwayDarkIcon,
                 alt: "Railway",
-                href: "https://railway.app",
+                href: "https://railway.app/?referralCode=shamsup",
               },
               {
                 src: "https://user-images.githubusercontent.com/1500684/158238105-e7279a0c-1640-40db-86b0-3d3a10aab824.svg",
